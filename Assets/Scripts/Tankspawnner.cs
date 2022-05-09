3d6fef68dab8f5be30b3e3ee3fdf54c2c0d8501d
GamedevDeadend <97394300+GamedevDeadend@users.noreply.github.com>
2022-05-09 18:19:01 +0530

    Initial commit

---
+++ Assets/Scripts/Tankspawnner.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Tankspawnner : MonoBehaviour
+{
+    public GameObject Tank;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(Tank, transform.position, Quaternion.identity);
+    }
+}
