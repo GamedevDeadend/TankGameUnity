8d72dfb856f49517ff01fe4c36f2abe725c5f0ed
GamedevDeadend <97394300+GamedevDeadend@users.noreply.github.com>
2022-05-14 02:14:59 +0530

    Added Game ui
    Added Game UI to select different type of tanks.

---
+++ Assets/Scripts/TankSelection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSelection : MonoBehaviour
+{
+    public TankSpawnner tankSpawnner;
+    public void BlueTankSelected()
+    {
+        // Debug.Log("Working");
+        tankSpawnner.CreateTank(TankTypes.BlueTank);
+        this.gameObject.SetActive(false);
+    }
+    public void GreenTankSelected()
+    {
+        tankSpawnner.CreateTank(TankTypes.GreenTank);
+        this.gameObject.SetActive(false);
+
+    }
+    public void RedTankSelected()
+    {
+        tankSpawnner.CreateTank(TankTypes.RedTank);
+        this.gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/Tankspawnner.cs
+++ Assets/Scripts/Tankspawnner.cs
@@ -19,12 +19,26 @@ public class TankSpawnner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        CreateTank();
+        
     }
 
-    public void CreateTank()
+    public void CreateTank(TankTypes tanktypes)
     {
-        TankModel tankModel = new TankModel(tanklist[2].movementspeed, tanklist[2].rotationspeed, tanklist[2].tanktypes, tanklist[2].color);
-        TankController tankController = new TankController(tankModel, tankview);
+        if (tanktypes == TankTypes.BlueTank)
+        {
+            TankModel tankModel = new TankModel(tanklist[1].movementspeed, tanklist[1].rotationspeed, tanklist[1].tanktypes, tanklist[1].color);
+            TankController tankController = new TankController(tankModel, tankview);
+        }
+
+        else if (tanktypes == TankTypes.GreenTank)
+        {
+            TankModel tankModel = new TankModel(tanklist[0].movementspeed, tanklist[0].rotationspeed, tanklist[0].tanktypes, tanklist[0].color);
+            TankController tankController = new TankController(tankModel, tankview);
+        }
+        else if (tanktypes == TankTypes.RedTank)
+        {
+            TankModel tankModel = new TankModel(tanklist[2].movementspeed, tanklist[2].rotationspeed, tanklist[2].tanktypes, tanklist[2].color);
+            TankController tankController = new TankController(tankModel, tankview);
+        }
     }
 }
